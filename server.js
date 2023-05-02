9cc36fb214d377902be650a86cade6fb1d3f5d6e
Oksana <your@email.com>
2023-05-02 20:06:37 +0200

    added

---
+++ server.js
@@ -0,0 +1,40 @@
+/** @format */
+
+import express from "express";
+import { resolve } from "path";
+import products from "./products.js";
+
+const app = express();
+const PORT = 5005;
+// console.log(products);
+const __dirname = resolve();
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  // res.send(`Welcome to the page I call home`);
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/about", (req, res) => {
+  // option 1
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      result: products,
+    },
+  });
+  // option 2
+  // res.json(products);
+  // option 3
+  //   res.send(products);
+});
+
+app.get("/contact", (req, res) => {
+  res.send(__dirname);
+});
+
+app.listen(PORT, () => {
+  console.log("Server listening on port:", PORT);
+});
